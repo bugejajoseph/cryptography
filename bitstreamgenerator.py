454ce67a0f65ebac1921032387f353e1218dee1e
bugejajoseph <bugejajoseph@yahoo.com>
2024-10-13 14:57:08 +0200

    Add files via upload

---
+++ bitstreamgenerator.py
@@ -0,0 +1,6 @@
+state = (1 << 127) | 1
+while True:
+        print(state & 1, end = '')
+        newbit = (state ^ (state >> 1) ^ (state >> 2) ^ (state >> 7)) & 1
+        state = (state >> 1) | (newbit << 127)
+        
